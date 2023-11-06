2b62152e9f2026f61118056cea66ee5671f6789a
DiegoGoncalves92 <goncalves.silva0903@gmail.com>
2023-11-05 21:16:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+function tocar_tecla_pom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocar_tecla_pom;
+
+function tocar_tecla_clap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocar_tecla_clap;
+
+function tocar_tecla_tim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocar_tecla_tim;
+
+function tocar_tecla_puff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocar_tecla_puff;
+
+function tocar_tecla_splash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocar_tecla_splash;
+
+function tocar_tecla_toim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocar_tecla_toim;
+
+function tocar_tecla_psh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocar_tecla_psh;
+
+function tocar_tecla_tic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocar_tecla_tic;
+
+function tocar_tecla_tom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocar_tecla_tom;
